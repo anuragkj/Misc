95cf4f805d33dc5d5eec430f98e04b1c0d715d93
Anurag Kumar Jha <f20200128@dubai.bits-pilani.ac.in>
2022-06-30 21:28:32 +0400

    Update Hacker_Rank_3D_Surface.py

--- Competitive/Hacker_Rank_3D_Surface.py
+++ Competitive/Hacker_Rank_3D_Surface.py
@@ -16,29 +16,46 @@ import sys
 
 def surfaceArea(A,H,W):
     zeroes = 0
-    x_maxm = []
-    for i in A:
-        x_maxm.append(max(i))
-        for j in i:
-            if j == 0:
-              zeroes+=1
-    sum_1 = (H*W-zeroes)
-    sum_2 = 0
-    sum_3 = 0
-    y_maxm = []
-    x_gaps = []
-    y_gaps = []
+    y_maxm = 0
+    x_gaps = 0
+    y_gaps = 0
+    x_maxm = 0
     for j in range(W):
         maxm = 0
+        gap_store = 0
         for i in range(H):
+            if(A[i][j] == 0):
+                zeroes+=1
             if A[i][j]>maxm:
                 maxm = A[i][j]
-            # if (not(i==0 or j == 0 or i == H-1 or j == W-1)):
-            #     if A[i][j]<A[i+1][j] and A[i][j]<A[i-1][j]
+            
+            if(not(i==0) and A[i][j]<=A[i][j-1]):
+                print(i,j)
+                gap_store+=A[i][j-1]-A[i][j]
+            else:
+                gap_store+=A[i][j]-A[i][j-1]
+                y_gaps+=gap_store
+                gap_store = 0
+        y_maxm+=maxm
                     
-        y_maxm.append(maxm)
-    
-    print(2*(sum_1 + sum(x_maxm) + sum(y_maxm)))
+    for i in range(H):
+        maxm = 0
+        gap_store = 0
+        for j in range(W):
+            if A[i][j]>maxm:
+                maxm = A[i][j]
+            
+            if(not(j==0) and A[i][j]<=A[i-1][j]):
+                print(i,j)
+                gap_store+=A[i-1][j]-A[i][j]
+            else:
+                gap_store+=A[i][j]-A[i-1][j]
+                x_gaps+=gap_store
+                gap_store = 0
+        x_maxm+=maxm
+        
+    sum_1 = (H*W-zeroes)
+    print(f"sum_1: {sum_1}, x_maxm: {x_maxm}, y_maxm: {y_maxm}, x_gaps: {x_gaps}, y_gaps : {y_gaps}")
     return(1)
 
 if __name__ == '__main__':
