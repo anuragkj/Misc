a7dd089a91329cfec87ac7e34754720e7326f4ea
Anurag Kumar Jha <f20200128@dubai.bits-pilani.ac.in>
2022-06-30 21:43:46 +0400

    Update Hacker_Rank_3D_Surface.py

--- Competitive/Hacker_Rank_3D_Surface.py
+++ Competitive/Hacker_Rank_3D_Surface.py
@@ -14,49 +14,29 @@ import sys
 # The function accepts 2D_INTEGER_ARRAY A as parameter.
 #
 
-def surfaceArea(A,H,W):
-    zeroes = 0
-    y_maxm = 0
-    x_gaps = 0
-    y_gaps = 0
-    x_maxm = 0
-    for j in range(W):
-        maxm = 0
-        gap_store = 0
-        for i in range(H):
-            if(A[i][j] == 0):
-                zeroes+=1
-            if A[i][j]>maxm:
-                maxm = A[i][j]
-            
-            if(not(i==0) and A[i][j]<=A[i][j-1]):
-                print(i,j)
-                gap_store+=A[i][j-1]-A[i][j]
-            else:
-                gap_store+=A[i][j]-A[i][j-1]
-                y_gaps+=gap_store
-                gap_store = 0
-        y_maxm+=maxm
-                    
-    for i in range(H):
-        maxm = 0
-        gap_store = 0
-        for j in range(W):
-            if A[i][j]>maxm:
-                maxm = A[i][j]
-            
-            if(not(j==0) and A[i][j]<=A[i-1][j]):
-                print(i,j)
-                gap_store+=A[i-1][j]-A[i][j]
-            else:
-                gap_store+=A[i][j]-A[i-1][j]
-                x_gaps+=gap_store
-                gap_store = 0
-        x_maxm+=maxm
-        
-    sum_1 = (H*W-zeroes)
-    print(f"sum_1: {sum_1}, x_maxm: {x_maxm}, y_maxm: {y_maxm}, x_gaps: {x_gaps}, y_gaps : {y_gaps}")
-    return(1)
+def surfaceArea(A):
+    area = 0
+    neighs = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+
+    for x in range(H):
+        for y in range(W):
+            el = A[x][y]
+            # add base and top area
+            area += 2
+            for dx, dy in neighs:
+                try:
+                    # Raise index exception if trying to access index -1
+                    if -1 in [x+dx, y+dy]:
+                        raise IndexError
+
+                    # Add area of actual element from neighbours as base
+                    new = el - A[x+dx][y+dy]
+                    if new > 0:
+                        area += new
+                # Add area corresponding to the sides of the figure
+                except IndexError:
+                    area += el
+    return (area)
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
@@ -72,7 +52,7 @@ if __name__ == '__main__':
     for _ in range(H):
         A.append(list(map(int, input().rstrip().split())))
 
-    result = surfaceArea(A,H,W)
+    result = surfaceArea(A)
 
     fptr.write(str(result) + '\n')
 
